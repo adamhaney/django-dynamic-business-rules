aa950ff822666e0b044b5da201d98b3fc181e5a4
John Vandenberg <jayvdb@gmail.com>
2020-09-09 15:07:00 +0700

    Delay importation of models
    Fixes https://github.com/adamhaney/django-dynamic-business-rules/issues/3

--- dynamic_business_rules/apps.py
+++ dynamic_business_rules/apps.py
@@ -1,11 +1,11 @@
 from django.apps import AppConfig
 
-from .models import BusinessRuleSet
-
 
 class DynamicBusinessRulesConfig(AppConfig):
     name = 'dynamic_business_rules'
 
     def ready(self):
+        from .models import BusinessRuleSet
+
         for ruleset in BusinessRuleSet.objects.all:
             ruleset.signal_object.connect(ruleset.signal_callback)
